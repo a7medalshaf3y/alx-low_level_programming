2417dff7e2036374bfef0778fc6b3d3724732b6a
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-11 23:36:47 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: A c program that prints with printf function
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	pfintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
