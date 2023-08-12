765d53b6734b815eb8f82b81db0c849aa3ac198a
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-11 22:41:28 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
+/**
+ *main - entry point
+ * Decription : a c program that print using put function
+ * return : 0 (success)
+ * /
+
 #include <stdio.h>
-int main(void){
+int main(void)
+{
     puts("\"Programming is like building a multilingual puzzle");
     return(0);
 }
