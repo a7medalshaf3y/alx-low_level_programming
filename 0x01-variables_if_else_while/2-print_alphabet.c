e991762ab732010d32f1b14f579e28867e685dc8
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-14 17:53:16 -0700

    a

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,20 +11,12 @@
 int main(void)
 {
 	char ch = 'a';
-	char CH = 'A';
 
 	/*prints a - z*/
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
-	}
-
-	/*print A - Z*/
-	while (CH <= 'Z')
-	{
-		putchar(CH);
-		CH++;
 	}
 	putchar('\n');
 
