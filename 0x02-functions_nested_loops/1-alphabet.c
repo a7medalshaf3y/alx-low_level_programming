04fd20e5995662e43c04a0af674a4ba4064d6bca
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-15 16:59:33 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * print alphabet from a to z using _putchar function
+ *
+*/
+
+void print_alphabet(void);
+
+{
+
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+}
