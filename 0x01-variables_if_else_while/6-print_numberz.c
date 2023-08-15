3f317174857393b3d2dc12992ec75f1295eb5dd9
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-14 18:47:04 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * Description: print all digits on base 10
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+		digit++;
+	}
+	putchar("\n");
+	return (0);
+}
