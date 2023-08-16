959048ad5a3c61d8c5ef029421c50bc05f19ce8c
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-15 17:23:45 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that print alphabet 10 times
+*/
+
+void print_alphabet_x10(void);
+{
+
+	int line, ch;
+
+	for (ch = 'a'; line <= 9; line++)
+		_putchar(ch);
+	_putchar('\n');
+
+}
