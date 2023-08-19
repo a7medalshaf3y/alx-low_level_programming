fe6124a36efe0ee6476c59c272dcb99b5b6f364e
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-18 19:18:50 -0700

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 - 14 ten times
+ * 		and only use _putchar three times
+ *
+ * Return: Always 0 (success)
+ *
+*/
+
+void more_numbers(void)
+{
+	int num, row, count;
+
+	for (row = 1:
+
+
+
+
+}
