c8240d3661fe05a28c9f13a042cff244fd9296b1
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-18 18:37:49 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *
+ * only using _putchar twice
+ *
+ * Return: 0 (success)
+ *
+*/
+
+void print_numbers(void)
+{
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
+
+
+
+
+
+
+}
