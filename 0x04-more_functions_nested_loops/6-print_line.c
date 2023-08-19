fb7cadaac038b615f174e3220d6c944c8449ea35
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-18 19:39:17 -0700

    a

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,18 +3,18 @@
 /**
  * print_line - print a straight line
  *
- * @n: is the number of times the - character should be printed
+ * @n: is the number of times the _ character should be printed
 */
 
 void print_line(int n)
 {
-	int lnchr;
+	int lnChr;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (lnchr = 1; lnchr <= n; lnchr++)
+		for (lnChr = 1; lnChr <= n; lnChr++)
 			_putchar('_');
 		_putchar('\n');
 	}
