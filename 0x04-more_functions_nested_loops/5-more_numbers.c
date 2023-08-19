01f836697abdb6f5f0e26da135f809781106138f
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-18 19:22:55 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - print 0 - 14 ten times
- * 		and only use _putchar three times
+ *		and only use _putchar three times
  *
  * Return: Always 0 (success)
  *
@@ -12,7 +12,17 @@ void more_numbers(void)
 {
 	int num, row, count;
 
-	for (row = 1:
+	for (row = 1; row <= 10; count++)
+	{
+		num = count;
+		if (count > 9)
+		{
+			_putchar(1 + 48);
+			num = count % 10;
+		}
+		_putchar(num + 48);
+	}
+}
 
 
 
