dd974ba4502a035bbd50f9e42d1d5208a14b8670
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-18 18:52:15 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,9 +11,9 @@ void print_numbers(void)
 {
 	int num = 0;
 
-	do {
+	for (num = 0, num <= 9 && num >= 0, num++)
+	{
 		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9)
-	_putchar('\n')
+		_putchar('\n');
+	}
 }
