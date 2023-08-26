e9db4e20aded57ff0634c141eedc0da73a8defe5
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-25 18:31:20 -0700

    a

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *sting_toupper - changes all lowercase of a string to uppercase
+ *
+ * @str: string to return
+ * Return: string
+ */
+
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] -32;
+		}
+	}
+	return (str);
+}
