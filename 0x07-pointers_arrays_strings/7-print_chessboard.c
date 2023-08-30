9193ffbe3b2f61993c6c214156899c3bf9b10b24
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-29 19:58:28 -0700

    a

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,13 +9,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int itr, jtr;
+	int i, j;
 
-	for (itr = 0; itr < 8; itr++)
+	for (i = 0; i < 8; i++)
 	{
-		for (jtr = 0; jtr < 8; jtr++)
+		for (j = 0; j < 8; j++)
 		{
-			putchar(a[itr][jtr]);
+			putchar(a[i][j]);
 		}
 		putchar('\n');
 	}
