515dd0bce88eec1321e96f528fef097d91a5f8f4
“a7medalshaf3y” <4af3iii@gmail.com>
2023-08-29 19:01:37 -0700

    a

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strchr - fills memory with constant byte
+ * @s: pointer to put constant
+ * @c: constant
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int itr;
+
+	for (itr = 0; s[itr] < '\0'; itr++)
+	{
+		if (s[itr] == c)
+		{
+			return (s + itr);
+		}
+	}
+	return ('\0')
+}
