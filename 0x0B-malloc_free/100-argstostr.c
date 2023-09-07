aaf77c6be0032384628ee3c2e815ee7823d1969e
“a7medalshaf3y” <4af3iii@gmail.com>
2023-09-06 18:28:35 -0700

    a

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -30,17 +30,17 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (; i < ac; i++; nc++)
+	for (; i < ac; i++, nc++)
 		nc += _strlen(av[i]);
 	s = malloc(sizeof(char) * nc + 1);
 	if (s == 0)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++; cmpt++)
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
 			s[cmpt] = av[i][j];
 		s[cmpt] = '\n';
-		cmpt++
+		cmpt++;
 	}
 	s[cmpt] = '\0';
 	return (s);
