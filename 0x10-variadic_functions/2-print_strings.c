857535469b9ec6452944cccf49cc59c826b450a5
“a7medalshaf3y” <4af3iii@gmail.com>
2023-09-15 18:56:54 -0700

    a

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_string - print string with separator
+ * print_strings - print string with separator
  * @separator: the string separator
  * @n: the number of arguments
  * @...: the string to print
